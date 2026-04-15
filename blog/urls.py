85d0403d5d8578cd94fa408c67e4464e8732936b
Pravodelova Aleksandra <a.pravodelova02@mail.ru>
2026-04-15 19:18:10 +0500

    Fix PEP8 issues: formatting and unused imports

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,16 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('post/<int:id>/', views.detail, name='detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path(
+        '',
+        views.index,
+        name='index'),
+    path(
+        'post/<int:id>/',
+        views.detail,
+        name='detail'),
+    path(
+        'category/<slug:category_slug>/',
+        views.category_posts,
+        name='category_posts'),
 ]
