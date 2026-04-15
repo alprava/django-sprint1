0c5b13553e1644aa7d3fb242dd2c50c69c905969
Pravodelova Aleksandra <a.pravodelova02@mail.ru>
2026-04-15 19:43:57 +0500

    Fix quotes in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path(posts/<int:id>/', views.detail, name='post_detail'),
-    path(
-        'post/<int:id>/',
-        views.detail,
-        name='detail'),
-    path(
-        'category/<slug:category_slug>/',
-        views.category_posts,
-        name='category_posts'),
+    path('', views.index, name='index'),
+    path('posts/<int:id>/', views.detail, name='post_detail'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
 ]
